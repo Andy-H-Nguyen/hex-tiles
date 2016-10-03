457fe528f5c3bc976102045e4dfe4117c997f8b6
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2016-10-03 17:05:53 +0000

    commit changes

--- js/main.js
+++ js/main.js
@@ -3,10 +3,15 @@ var GameState = {
   //load the game assets before the game starts
   preload: function() {
     this.load.image('background', 'assets/images/background.png');
-    
+    this.load.image('chicken', 'assets/images/chicken.png');
+    this.load.image('horse', 'assets/images/horse.png');
+    this.load.image('pig', 'assets/images/pig.png');
+    this.load.image('sheep', 'assets/images/sheep.png');
   },
   //executed after everything is loaded
   create: function() {
+    this.background = this.game.add.sprite(0, 0, 'background');
+    this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
   },
   //this is executed multiple times per second
   update: function() {
